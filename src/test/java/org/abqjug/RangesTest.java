a05be158b18b16cfcaaa1b89f54fb31d6b497a23
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2015-04-14 20:08:05 -0600

    Ranges added Apache, and added more Range Examples

--- src/test/java/org/abqjug/RangesTest.java
+++ src/test/java/org/abqjug/RangesTest.java
@@ -1,12 +1,12 @@
 /**
- * Copyright 2010-2013 Daniel Hinojosa
- *
+ * Copyright 2010-2015 Daniel Hinojosa
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,8 +22,29 @@ import static com.google.common.collect.Range.closed;
 import static org.fest.assertions.Assertions.assertThat;
 
 public class RangesTest {
+
+    /**
+     * Range Operations
+     * (a..b)	open(C, C)
+     * [a..b]	closed(C, C)
+     * [a..b)	closedOpen(C, C)
+     * (a..b]	openClosed(C, C)
+     * (a..+∞)	greaterThan(C)
+     * [a..+∞)	atLeast(C)
+     * (-∞..b)	lessThan(C)
+     * (-∞..b]	atMost(C)
+     * (-∞..+∞)	all()
+     */
     @Test
     public void testRanges() {
+        assertThat(Range.open(0, 12).contains(11)).isTrue();
+        assertThat(Range.open(0, 12).contains(12)).isFalse();
+        assertThat(Range.greaterThan(10).contains(13)).isTrue();
+        assertThat(Range.greaterThan(10).contains(1000)).isTrue();
+        assertThat(Range.greaterThan(10).contains(1)).isFalse();
+        assertThat(Range.open(0, 12).contains(12)).isFalse();
+        assertThat(Range.atMost(100).encloses(Range.closed(0, 10))).isTrue();
+
         assertThat(closed(3, 5).isConnected(Range.open(5, 10))).isEqualTo(true); // returns true
         assertThat(closed(0, 9).isConnected(closed(3, 4))).isEqualTo(true); // returns true
         assertThat(closed(0, 5).isConnected(closed(3, 9))).isEqualTo(true); // returns true
