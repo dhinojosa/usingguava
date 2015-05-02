73321ab89266b7bd62afd31b2cc3b27e40c2259c
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2015-05-02 10:20:38 -0400

    Cleaned up References Page

--- src/test/java/org/abqjug/ReferencesTest.java
+++ src/test/java/org/abqjug/ReferencesTest.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2010-2013 Daniel Hinojosa
- *
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,10 +18,10 @@ package org.abqjug;
 import org.testng.annotations.Test;
 
 import java.lang.ref.*;
-import java.util.WeakHashMap;
 
 public class ReferencesTest {
     public class RunWeakReference extends Thread {
+
         private ReferenceQueue<Integer> queue;
 
         public RunWeakReference(ReferenceQueue<Integer> queue) {
@@ -76,20 +76,20 @@ public class ReferencesTest {
     @Test(groups = "references")
     public void testWeakReference() throws InterruptedException {
         Integer f = 10;
-        WeakReference<Integer> weakReference = new WeakReference<Integer>(f);
+        WeakReference<Integer> weakReference = new WeakReference<>(f);
         System.out.println(weakReference.get());
         f = null;
         System.gc();
+        Thread.sleep(10000);
         System.out.println(weakReference.get());
     }
 
-     @Test(groups = "references")
+    @Test(groups = "references")
     public void testSoftReference() throws InterruptedException {
-        Integer f = 10;
-        SoftReference<Integer> softReference = new SoftReference<Integer>(f);
+        SoftReference<Object> softReference = new SoftReference<>(new Object());
         System.out.println(softReference.get());
-        f = null;
         System.gc();
+        Thread.sleep(10000);
         System.out.println(softReference.get());
     }
 }
